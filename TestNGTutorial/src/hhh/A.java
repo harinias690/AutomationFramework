548c8b041d5e37269818c0a45f18826990a56753
Harini Akula <harini.as690@gmail.com>
2019-08-23 11:55:25 -0400

    Commiting working code

---
+++ TestNGTutorial/src/hhh/A.java
@@ -0,0 +1,17 @@
+package hhh;
+
+import org.testng.annotations.Test;
+
+public class A {
+	
+	@Test
+	public void login() {
+		System.out.println("Login successful");
+	}
+	
+	@Test
+	public void displayCustomerDetails() {
+		System.out.println("Customer details");
+	}
+
+}
